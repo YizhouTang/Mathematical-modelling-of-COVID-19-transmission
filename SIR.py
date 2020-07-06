81516abdf1bcd9ff24d05a5989339ecfe4763a20
YizhouTang <34849408+YizhouTang@users.noreply.github.com>
2020-07-06 12:46:35 -0400

    LambertW implemention + more visualization

--- SIR.py
+++ SIR.py
@@ -14,16 +14,17 @@ class SIR():
 
     def intSIR(self):
         return odeint(deriv,self.y, self.t, args=(self.N, self.beta, self.gamma)).T
-    def plot(self):
+
+    def plot_normalized_curves(self):
         S,I,R = self.intSIR()
 
         fig = plt.figure(facecolor='w')
         ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-        ax.plot(self.t, S / 1000, 'b', alpha=0.5, lw=2, label='Susceptible')
-        ax.plot(self.t, I / 1000, 'r', alpha=0.5, lw=2, label='Infected')
-        ax.plot(self.t, R / 1000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+        ax.plot(self.t, S / self.N, 'b', alpha=0.5, lw=2, label='Susceptible')
+        ax.plot(self.t, I / self.N, 'r', alpha=0.5, lw=2, label='Infected')
+        ax.plot(self.t, R / self.N, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
         ax.set_xlabel('Time /days')
-        ax.set_ylabel('Number (1000s)')
+        ax.set_ylabel('Normalized')
         ax.set_ylim(0, 1.2)
         ax.yaxis.set_tick_params(length=0)
         ax.xaxis.set_tick_params(length=0)
@@ -34,7 +35,28 @@ class SIR():
             ax.spines[spine].set_visible(False)
         plt.show()
 
+    def plot_individual_curves(self):
+        S,I,R = self.intSIR()
+        plt.figure()
+        plt.plot(self.t, S, 'b', alpha=0.5, lw=2, label='Susceptible')
+        plt.xlabel("dates")
+        plt.ylabel("count")
+        plt.legend()
+        plt.show()
 
+        plt.figure()
+        plt.plot(self.t, I, 'r', alpha=0.5, lw=2, label='Infected')
+        plt.xlabel("dates")
+        plt.ylabel("count")
+        plt.legend()
+        plt.show()
+
+        plt.figure()
+        plt.plot(self.t,R, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+        plt.xlabel("dates")
+        plt.ylabel("count")
+        plt.legend()
+        plt.show()
 
 def deriv(y,t,N,beta,gamma):
     S,I,R = y
--- testing_environment.py
+++ testing_environment.py
@@ -1,20 +1,78 @@
 from SIR import SIR
 import numpy as np
+import pandas as pd
+from scipy.optimize import minimize
+from scipy.integrate import solve_ivp
+from scipy.special import lambertw
+import matplotlib.pyplot as plt
+df = pd.read_csv('data.csv')#,parse_dates = True,index_col='date')
+df.ffill(inplace = True)
+confirmed = df['numconf'].values
+recovered = df['numrecover'].values + df['numdeaths'].values
+
+plt.figure()
+plt.plot(confirmed)
+plt.plot(recovered)
+plt.legend(['Infected','Recovered'])
+plt.title("Canada COVID-10 Stats")
+plt.show()
+
+
 # Total population, N.
-N = 1000
+N = 37590000
 # Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 = 1, 0
+I0, R0 = confirmed[0], recovered[0]
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0
 
-# Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.2, 1./10
+
+data = N - confirmed- recovered
+
+def loss(point, data,infected, recovered, s_0, i_0, r_0):
+    size = len(infected)
+    beta, gamma = point
+    def SIR(t, y):
+        S = y[0]
+        I = y[1]
+        R = y[2]
+        return [-beta*S*I/N, beta*S*I/N-gamma*I, gamma*I]
+    solution = solve_ivp(SIR, [0, size], [s_0,i_0,r_0], t_eval=np.arange(0, size, 1), vectorized=True)
+    l0 = np.sqrt(np.mean((solution.y[0] - data)**2))
+    l1 = np.sqrt(np.mean((solution.y[1] - infected)**2))
+    l2 = np.sqrt(np.mean((solution.y[2] - recovered)**2))
+    #print(l0,l1,l2)
+    #alpha = 0.5
+    return l0+l1+l2
+
+
+optimal = minimize(loss, [0.001, 0.001], args=(data, confirmed,recovered, S0, I0, R0), method='L-BFGS-B', bounds=[(0.00000001, 1), (0.00000001, 1)])
+beta, gamma = optimal.x
+print("Beta: ",beta)
+print("Gamma: ",gamma)
+
 # A grid of time points (in days)
-t = np.linspace(0, 160, 160)
+t = np.linspace(0, 365, 365)
 
 # Initial conditions vector
 y0 = S0, I0, R0
 # Initialize model
 mdl = SIR(y0,t,N,beta,gamma)
 #Plot
-mdl.plot()
+mdl.plot_normalized_curves()
+mdl.plot_individual_curves()
+
+
+#####LambertW section
+ratio = gamma/beta
+plt.plot()
+ratio = [x/100 for x in range(1,200)]
+
+uf = []
+for i in ratio:
+    sol = (lambertw(-999 / 1000 * i * np.exp(-i)) / i + 1)
+    uf.append(np.real(sol))
+plt.figure()
+plt.plot(ratio,uf)
+plt.title("Phase Transition at gamma/beta = 1")
+plt.xlabel("gamma/beta")
+plt.show()
